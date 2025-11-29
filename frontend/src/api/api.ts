a16995a4abb1d6fffdfe040e78d67cc9f1064ae6
sehee0207 <lolry0207@gmail.com>
2025-11-30 00:34:52 +0900

    엔드포인트에 api 붙이기(임시)

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@
 import ky from 'ky'
 
 export const api = ky.create({
-    prefixUrl: import.meta.env.VITE_API_BASE_URL,
+    prefixUrl: `${import.meta.env.VITE_API_BASE_URL}/api`,
     credentials: 'include',
     timeout: 180000,
     hooks: {
